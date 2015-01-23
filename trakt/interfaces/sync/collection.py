e5517895a45a65ea677e659bf8f7dae07826cb16
Marco Amador <marco_azb@sapo.pt>
2015-01-23 20:16:30 +0100

    Continuing interfaces code.

--- trakt/interfaces/sync/collection.py
+++ trakt/interfaces/sync/collection.py
@@ -4,3 +4,53 @@ from trakt.interfaces.sync.core.mixins import Get, Add, Remove
 class SyncCollectionInterface(Get, Add, Remove):
     path = 'sync/collection'
     flags = {'is_collected': True}
+    
+    @authenticated
+    def get(self, media, store=None):   
+        return super(SyncCollectionInterface, self).get(media, store)
+            
+    @authenticated
+    def add(self, shows=None, movies=None, episodes=None):  
+        if not shows and not movies and not episodes:
+            raise ValueError('Missing media items')
+            
+        data = {}
+        
+        if movies:
+            data['movies'] = movies
+        elif shows:
+            data['shows'] = shows
+        elif episodes:
+            data['episodes'] = episodes
+            
+        return super(SyncCollectionInterface, self).add(data)
+        
+        
+    @authenticated
+    def remove(self, shows=None, movies=None, episodes=None):
+        if not shows and not movies and not episodes:
+            raise ValueError('Missing media items to remove')
+            
+        data = {}
+        
+        if movies:
+            data['movies'] = movies
+        elif shows:
+            data['shows'] = shows
+        elif episodes:
+            data['episodes'] = episodes
+            
+        return super(SyncCollectionInterface, self).remove(data)
+        
+        
+    #
+    #    Shortcut methods
+    #
+    
+    @authenticated
+    def getShows(self, store=None):
+        return self.get('shows', store)
+
+    @authenticated
+    def getMovies(self, store=None):
+        return self.get('movies', store)
