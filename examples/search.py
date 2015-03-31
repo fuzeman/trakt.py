158b9be6b994d80a57b3502ed3b7f71ad70a7e9e
Dean Gardiner <gardiner91@gmail.com>
2015-03-31 13:26:23 +1300

    Updated [examples/search.py]

--- examples/search.py
+++ examples/search.py
@@ -4,6 +4,33 @@ import os
 logging.basicConfig(level=logging.DEBUG)
 
 from trakt import Trakt
+from trakt.objects import Season, Episode
+
+
+def print_lookup(id, service):
+    print "Trakt['search'].lookup(%r, %r)" % (id, service)
+
+    item = Trakt['search'].lookup(id, service)
+
+    if type(item) is Episode and item.show:
+        sk, ek = item.pk
+        print "\t%s (%s) - S%02dE%02d %r" % (item.show.title, item.show.year, sk, ek, item.title)
+    else:
+        print "\t%s (%s)" % (item.title, item.year)
+
+
+def print_query(query, media=None, year=None):
+    print "Trakt['search'].query(%r, %r, %r)" % (query, media, year)
+
+    items = Trakt['search'].query(query, media, year)
+
+    for item in items:
+        if type(item) is Episode and item.show:
+            sk, ek = item.pk
+            print "\t[%.2d%%] %s (%s) - S%02dE%02d %r" % (item.score, item.show.title, item.show.year, sk, ek, item.title)
+        else:
+            print "\t[%.2d%%] %s (%s)" % (item.score, item.title, item.year)
+
 
 if __name__ == '__main__':
     # Configure
@@ -12,12 +39,11 @@ if __name__ == '__main__':
     )
 
     # Lookup by id
-    l_movie = Trakt['search'].lookup('tt0848228', 'imdb')
-    l_show = Trakt['search'].lookup('tt0903747', 'imdb')
-    l_episode = Trakt['search'].lookup('tt0959621', 'imdb')
+    print_lookup('tt0848228', 'imdb')
+    print_lookup('tt0903747', 'imdb')
+    print_lookup('tt0959621', 'imdb')
 
     # Search by name
-    s_movie = Trakt['search'].query('The Avengers', 'movie')
-    s_show = Trakt['search'].query('Breaking Bad', 'show')
-
-    pass
+    print_query('The Avengers', 'movie')
+    print_query('Breaking Bad', 'show')
+    print_query('Breaking Bad', 'episode')
