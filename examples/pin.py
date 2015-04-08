b6e95f57c2a02964aeb9641affa4262929ba5a9b
Dean Gardiner <gardiner91@gmail.com>
2015-04-08 15:36:34 +1200

    Updated [examples/pin.py] with token refreshing

--- examples/pin.py
+++ examples/pin.py
@@ -5,6 +5,57 @@ logging.basicConfig(level=logging.DEBUG)
 
 from trakt import Trakt
 
+
+class Application(object):
+    def __init__(self):
+        self.authorization = None
+
+        # Bind trakt events
+        Trakt.on('oauth.token_refreshed', self.on_token_refreshed)
+
+    def run(self):
+        self.authenticate()
+
+        if not self.authorization:
+            print 'Authentication required'
+            exit(1)
+
+        # Simulate expired token
+        self.authorization['expires_in'] = 0
+
+        # Test authenticated calls
+        with Trakt.configuration.oauth.from_response(self.authorization):
+            # Expired token, requests will return `None`
+            print Trakt['sync/collection'].movies()
+
+        with Trakt.configuration.oauth.from_response(self.authorization, refresh=True):
+            # Expired token will be refreshed automatically (as `refresh=True`)
+            print Trakt['sync/collection'].movies()
+
+        with Trakt.configuration.oauth.from_response(self.authorization):
+            # Current token is still valid
+            print Trakt['sync/collection'].movies()
+
+    def authenticate(self):
+        # Request authentication
+        print 'Navigate to %s' % Trakt['oauth'].pin_url()
+        pin = raw_input('Pin: ')
+
+        # Exchange `code` for `access_token`
+        self.authorization = Trakt['oauth'].token_exchange(pin, 'urn:ietf:wg:oauth:2.0:oob')
+
+        if not self.authorization:
+            return False
+
+        print 'Token exchanged - authorization: %r' % self.authorization
+        return True
+
+    def on_token_refreshed(self, response):
+        # OAuth token refreshed, save token for future calls
+        self.authorization = response
+
+        print 'Token refreshed - authorization: %r' % self.authorization
+
 if __name__ == '__main__':
     # Configure
     Trakt.base_url = 'http://api.staging.trakt.tv'
@@ -18,27 +69,6 @@ if __name__ == '__main__':
         secret=os.environ.get('CLIENT_SECRET')
     )
 
-    # Request authentication
-    print 'Navigate to %s' % Trakt['oauth'].pin_url()
-    pin = raw_input('Pin: ')
-
-    # Exchange `code` for `access_token`
-    details = Trakt['oauth'].token_exchange(pin, 'urn:ietf:wg:oauth:2.0:oob')
-
-    if not details:
-        print 'Token exchange failed'
-        exit(1)
-
-    access_token = details.get('access_token')
-    refresh_token = details.get('refresh_token')
-
-    # Test authentication
-    print 'Using access_token: %r' % access_token
-
-    with Trakt.configuration.oauth(access_token):
-        print Trakt['sync/collection'].movies()
-
-    # Refresh authentication with `refresh_token`
-    raw_input('[token_refresh]')
-
-    details = Trakt['oauth'].token_refresh(refresh_token, 'urn:ietf:wg:oauth:2.0:oob')
+    # Start application
+    app = Application()
+    app.run()
