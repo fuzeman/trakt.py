808a6d7507ce24fe331210edcd141b8784498afd
shad <kenny.shad7@gmail.com>
2015-09-22 22:46:21 -0400

    [TASK] Enable exceptions for movies and shows
    Add the capability to have exceptions raised when an error status code
    is returned by the trakt API.
    
    Also checking status before attempting to process the message payload
    is the appropriate order as error messages are not always parseable
    and in many cases should not be parsed if there was an error status
    returned.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,5 +1,7 @@
+from trakt.core import exceptions
 from trakt.interfaces.base import Interface
 
+import pytest
 import responses
 import requests
 
@@ -16,3 +18,31 @@ def test_content_type():
 
     data = Interface.get_data(r)
     assert data is None
+
+
+@responses.activate
+def test_server_error():
+    responses.add(
+        responses.GET, 'http://mock/test',
+        body='{"test": True}', status=502,
+        content_type=None
+    )
+
+    r = requests.get('http://mock/test')
+
+    with pytest.raises(exceptions.ServerError):
+        Interface.get_data(r, exceptions=True)
+
+
+@responses.activate
+def test_client_error():
+    responses.add(
+        responses.GET, 'http://mock/test',
+        body='{"test": True}', status=404,
+        content_type=None
+    )
+
+    r = requests.get('http://mock/test')
+
+    with pytest.raises(exceptions.ClientError):
+        Interface.get_data(r, exceptions=True)
