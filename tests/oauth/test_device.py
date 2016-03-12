77280ae1314fb6a140a1e3ff5aec0ac3a3af8316
Dean Gardiner <me@dgardiner.net>
2016-03-12 16:07:28 +1300

    Added tests for `Trakt['oauth/device']`, and additional tests for `Trakt['oauth']` and Trakt['oauth/pin']
    #46

---
+++ tests/oauth/test_device.py
@@ -0,0 +1,93 @@
+from trakt import Trakt
+import json
+import pytest
+import responses
+
+
+@responses.activate
+def test_code():
+    expected_request = {
+        'client_id': 'mock-client_id'
+    }
+
+    def callback(request):
+        data = json.loads(request.body)
+
+        if data != expected_request:
+            return 403, {}, ''
+
+        return 200, {}, json.dumps({
+            "device_code": "mock-device_code",
+            "user_code": "mock-user_code",
+            "verification_url": "https://trakt.tv/activate",
+            "expires_in": 600,
+            "interval": 5
+        })
+
+    responses.add_callback(
+        responses.POST, 'http://api.mock/oauth/device/code',
+        callback=callback,
+        content_type='application/json'
+    )
+
+    Trakt.base_url = 'http://api.mock'
+
+    # Validate `code` request/response
+    with Trakt.configuration.client('mock-client_id', 'mock-client_secret'):
+        assert Trakt['oauth/device'].code() == {
+            "device_code": "mock-device_code",
+            "user_code": "mock-user_code",
+            "verification_url": "https://trakt.tv/activate",
+            "expires_in": 600,
+            "interval": 5
+        }
+
+    # Ensure `code` raises a `ValueError` on incorrect configuration
+    with pytest.raises(ValueError):
+        assert Trakt['oauth/device'].code()
+
+
+@responses.activate
+def test_token():
+    expected_request = {
+        'client_id': 'mock-client_id',
+        'client_secret': 'mock-client_secret',
+
+        'code': 'mock-device_code'
+    }
+
+    def callback(request):
+        data = json.loads(request.body)
+
+        if data != expected_request:
+            return 403, {}, ''
+
+        return 200, {}, json.dumps({
+            "access_token": "mock-access_token",
+            "token_type": "bearer",
+            "expires_in": 7200,
+            "refresh_token": "mock-refresh_token",
+            "scope": "public"
+        })
+
+    responses.add_callback(
+        responses.POST, 'http://api.mock/oauth/device/token',
+        callback=callback,
+        content_type='application/json'
+    )
+
+    Trakt.base_url = 'http://api.mock'
+
+    # Validate `token` request/response
+    with Trakt.configuration.client('mock-client_id', 'mock-client_secret'):
+        assert Trakt['oauth/device'].token('mock-device_code') == {
+            "access_token": "mock-access_token",
+            "token_type": "bearer",
+            "expires_in": 7200,
+            "refresh_token": "mock-refresh_token",
+            "scope": "public"
+        }
+
+    # Ensure `token` raises a `ValueError` on incorrect configuration
+    with pytest.raises(ValueError):
+        assert Trakt['oauth/device'].token('mock-device_code')
---
+++ tests/oauth/test_pin.py
@@ -0,0 +1,12 @@
+from tests.core.helpers import assert_url
+
+from trakt import Trakt
+import pytest
+
+
+def test_url():
+    with Trakt.configuration.app(id=1234):
+        assert_url(Trakt['oauth/pin'].url(), '/pin/1234')
+
+    with pytest.raises(ValueError):
+        Trakt['oauth/pin'].url()
