6f80ca36da8d266bc3987586f41021361c42aa98
Dean Gardiner <me@dgardiner.net>
2016-10-11 15:50:02 +1300

    Updated the `Trakt['sync/history']` interface to support the retrieval of history records
    #49

--- trakt/interfaces/sync/history.py
+++ trakt/interfaces/sync/history.py
@@ -1,5 +1,26 @@
-from trakt.interfaces.sync.core.mixins import Add, Remove
+from trakt.interfaces.sync.core.mixins import Get, Add, Remove
 
 
-class SyncHistoryInterface(Add, Remove):
+class SyncHistoryInterface(Get, Add, Remove):
     path = 'sync/history'
+
+    def get(self, media=None, store=None, params=None, page=1, per_page=10, **kwargs):
+        # Build query
+        query = {}
+
+        if page:
+            query['page'] = page
+
+        if per_page:
+            query['limit'] = per_page
+
+        # Request watched history
+        return super(SyncHistoryInterface, self).get(
+            media=media,
+            store=store,
+            params=params,
+
+            query=query,
+            flat=True,
+            **kwargs
+        )
